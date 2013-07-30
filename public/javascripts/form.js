a191e06e7ea849511403db6872a997ccb18f9372
Marius Cotofana <cmarius02@gmail.com>
2013-07-30 17:47:42 +0300

    Datepicker added and JQuery.

---
+++ public/javascripts/form.js
@@ -0,0 +1,8 @@
+$(document).ready(function () {                                  
+    $('.datepicker').datepicker({
+	format: "dd/mm/yyyy",
+    });
+
+    $('.selectpicker').selectpicker();
+});        
+
