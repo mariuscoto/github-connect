ad5e90a01a55f62fec13b48a6239d1dd320d92b3
Marius Cotofana <cmarius02@gmail.com>
2014-04-11 15:29:10 +0300

    Use images as background for divs.

--- app.js
+++ app.js
@@ -45,6 +45,7 @@ everyauth
 
 
 app.configure(function() {
+	//app.set('images', __dirname + '/public/images');
 	app.set('views', __dirname + '/views');
 	app.set('view engine', 'jade');
 	app.use(express.favicon("public/images/github-icon.ico"));
@@ -69,6 +70,7 @@ app.get('/idea-team', ideas.one);
 app.get('/idea-plan', ideas.one);
 app.get('/idea-plan-edit', ideas.one);
 app.get('/idea-settings', ideas.one);
+app.get('/idea/remove', ensureAuth, ideas.remove);
 app.post('/ideas', ideas.add);
 app.post('/idea/fav', ensureAuth, ideas.fav);
 app.post('/idea/unfav', ensureAuth, ideas.unfav);
@@ -77,7 +79,6 @@ app.post('/idea/flag', ensureAuth, ideas.flag);
 app.post('/ideas/comment', ensureAuth, ideas.comment);
 app.post('/idea-edit', ensureAuth, ideas.idea_edit);
 app.post('/idea-plan-edit', ensureAuth, ideas.idea_plan_edit);
-app.get('/idea/remove', ensureAuth, ideas.remove);
 
 app.get('/notifications', ensureAuth, ideas.notifications);
 
